d02d41ef95e8874a2071a0ee9d096f1caa612b33
Marcial Soto <marcialsotosanchez@gmail.com>
2018-09-13 16:40:47 -0500

    Update config.js

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   port: process.env.PORT || 8081,
   db: {
-    database: process.env.DB_NAME || 'dbtabtracker',
-    user: process.env.DB_USER || 'sa',
-    password: process.env.DB_PASS || 'Hacker147',
+    database: process.env.DB_NAME || 'database-name',
+    user: process.env.DB_USER || 'database-username',
+    password: process.env.DB_PASS || 'database-password',
     options: {
       dialect: process.env.DIALECT || 'mssql',
       host: process.env.HOST || 'localhost'
